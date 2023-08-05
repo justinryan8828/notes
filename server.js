15b22aa1ec769d61daeaa081b9ea258cc5397bd4
Justin Moore <justinmoore@Justins-MacBook-Air.local>
2023-08-04 20:21:17 -0400

    fixed a little ting

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const noteRoutes = require("./routes/noteRoutes");
 
 const app = express();
-const PORT = process.env.Port || 3001;
+const PORT = process.env.PORT || 3001;
 const htmlRouter = require("./routes/html");
 
 app.use(express.json());
